3948d4e4361dc49cce682a4d55eb143a2eae2ffa
Facualvarez <falvarezlongo@gmail.com>
2015-08-18 22:54:44 -0300

    alta categoria

--- Tarea1/src/Logica/ControladorCategoria.java
+++ Tarea1/src/Logica/ControladorCategoria.java
@@ -1,22 +1,44 @@
-
 package Logica;
 
-import javax.swing.JOptionPane;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.*;
+
 
 
 public class ControladorCategoria implements IControladorCategoria {
-    
-    private static ControladorCategoria instancia=null;
-    
-    public static ControladorCategoria getinstance(){
-        if (instancia==null)
+
+    private static ControladorCategoria instancia = null;
+    private Map<String, Categoria> ColeccionCategoria = new HashMap<String, Categoria>();
+
+    public static ControladorCategoria getinstance() {
+        if (instancia == null) {
             instancia = new ControladorCategoria();
+        }
         return instancia;
     }
-    
-    public void prueba(){
-        
-        JOptionPane.showMessageDialog(null,"Interactuo con funcion de la interfaz", "PRUEBA",JOptionPane.INFORMATION_MESSAGE);
+
+    public void addCategoria(String nombre, Categoria cat) {
+        ColeccionCategoria.put(nombre, cat);
     }
-            
+
+    public boolean verificoCat(String nombre) {
+        return ColeccionCategoria.containsKey(nombre);
+    }
+
+    public void AltaCategoria(String nombre) {
+
+        if (verificoCat(nombre)) {
+        Categoria cat = new Categoria(nombre);
+        addCategoria(nombre, cat);
+        }
+    }
+
+    
+    
+    public void prueba() {
+
+        JOptionPane.showMessageDialog(null, "Interactuo con funcion de la interfaz", "PRUEBA", JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
