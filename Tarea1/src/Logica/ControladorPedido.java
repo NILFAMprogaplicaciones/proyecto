69a4b1eec3f54b0593f4028ddc7431b074e5ae73
leo-gabriel <leonardogabriel_perez@hotmail.com>
2015-08-29 13:41:17 -0300

    Ver Pedido
    haciendo ver pedido, me falta hacer los controles del generar pedido

--- Tarea1/src/Logica/ControladorPedido.java
+++ Tarea1/src/Logica/ControladorPedido.java
@@ -9,8 +9,15 @@ public class ControladorPedido implements IControladorPedido {
     public void Caso_Generar_Pedido(DataPedido datapedido){
         ManejadorPedido MP=ManejadorPedido.getinstance();
         Pedido pedido=new Pedido(datapedido);
+        ProductosPedido pp=new ProductosPedido(pedido,datapedido.getColeccionProductosPedido());
         MP.addPedido(datapedido.getId(), pedido);
+        MP.addProductoPedido(pp.getPedido().getnum(), pp);
         JOptionPane.showMessageDialog(null,"Pedido Registrado","REGISTRO",JOptionPane.INFORMATION_MESSAGE); 
+    }
+    
+    public DataPedido Caso_Ver_Pedido(int id){
+        DataPedido p=null;
+        return p;
     }
     
 }
--- Tarea1/src/Logica/IControladorPedido.java
+++ Tarea1/src/Logica/IControladorPedido.java
@@ -6,4 +6,7 @@ public abstract interface IControladorPedido {
     
     public abstract void Caso_Generar_Pedido(DataPedido datapedido);
     
+    
+    public abstract DataPedido Caso_Ver_Pedido(int id);
+    
 }
