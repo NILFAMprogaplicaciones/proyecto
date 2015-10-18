48e008f66d53400c70935131ae6f4cf547b48da1
leo <leonardogabriel_perez@hotmail.com>
2015-10-18 21:31:33 -0200

    verificacion de lo ultimo 100%

--- Tarea1/test/Logica/RestauranteTest.java
+++ Tarea1/test/Logica/RestauranteTest.java
@@ -27,4 +27,30 @@ public class RestauranteTest {
         assertEquals(res.getcontraseña(),"contraseña");
     }
     
+    @Test
+    public void testBusquedaCPR() {
+        Fabrica fabrica=Fabrica.getInstance();
+        IControladorProducto ICP=fabrica.getIControladorProducto();
+        IControladorUsuario ICU=fabrica.getIControladorUsuario();
+        
+        //RESTAURANTE
+        Map<String,Categoria> colcat=new HashMap<>();
+        Categoria cat=new Categoria("cat1");
+        colcat.put(cat.getnombre(), cat);
+        Map<Integer,File> colfoto=new HashMap<>();
+        DataRestaurante datarestaurante=new DataRestaurante("contraseña","Pizzeria Mera","mera","mera@hotmail.com","Av 8 de octubre 2704",colcat,colfoto);
+        ICU.Caso_Registro_Restaurante(datarestaurante);
+        
+        Restaurante res=ICU.findRestaurante("mera");
+        File foto=new File("src/Imagenes/producto.jpg");
+        DataIndividual di=new  DataIndividual("producto", "", res,150, foto, "");
+        ICP.AltaProductoIndividual(di);
+        
+        assertEquals(res.BusquedaCategoria("ca"),true);
+        assertEquals(res.BusquedaProducto("prod"),true);
+        assertEquals(res.BusquedaRestaurante("mera"),true);
+        assertEquals(res.ExisteCategoria("cat1"),true);
+        assertEquals(res.ExisteCategoria("cat"),false);
+        assertEquals(res.getcontraseña(),"contraseña");
+    }
 }
